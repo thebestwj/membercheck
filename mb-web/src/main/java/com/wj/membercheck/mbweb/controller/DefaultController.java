f637212c5b71788efd9f2c1424ed2e3c0cb24a8d
thebestwj <wisestwang@163.com>
2019-10-08 17:08:18 +0800

    Optimize Member api Add Swagger2

--- mb-web/src/main/java/com/wj/membercheck/mbweb/controller/DefaultController.java
+++ mb-web/src/main/java/com/wj/membercheck/mbweb/controller/DefaultController.java
@@ -1,15 +1,9 @@
 package com.wj.membercheck.mbweb.controller;
 
-import com.wj.membercheck.biz.service.MemberService;
-import com.wj.membercheck.mbdao.model.MemberDO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by white_wolf on 2019/10/7.
@@ -18,41 +12,15 @@ import java.util.List;
  */
 @Controller
 @CrossOrigin
+@ResponseBody
 public class DefaultController {
 
-    @Autowired
-    MemberService memberService;
 
     @RequestMapping("/hello")
-    @ResponseBody
     public String hello(){
         return "hello";
     }
 
-    @ResponseBody
-    @RequestMapping("/amember")
-    public MemberDO amember(){
-        return memberService.findById(1);
-    }
 
-    @ResponseBody
-    @RequestMapping("/allmembers/{role}")
-    public List<MemberDO> allmembers(@PathVariable @NotNull String role){
-        return memberService.findAllByRole(role);
-    }
 
-    @ResponseBody
-    @PostMapping(value = "/addmember")
-    public int addMember(@NotNull @RequestBody(required = true) MemberDO memberDO){
-        System.out.println(memberDO.getMemName());
-        int newid =memberService.addOne(memberDO);
-        System.out.println(newid);
-        return newid;
-    }
-
-    @ResponseBody
-    @PostMapping(value = "/deletemember/{id}")
-    public int deleteMember(@PathVariable("id") int id){
-        return memberService.deleteById(id);
-    };
 }
---
+++ mb-web/src/main/java/com/wj/membercheck/mbweb/controller/MemberController.java
@@ -0,0 +1,54 @@
+package com.wj.membercheck.mbweb.controller;
+
+import com.wj.membercheck.biz.service.MemberService;
+import com.wj.membercheck.mbdao.model.MemberDO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Created by white_wolf on 2019/10/8.
+ *
+ * @author thebestwj
+ */
+@Controller
+@CrossOrigin
+@ResponseBody
+@Api("成员操作")
+public class MemberController {
+
+    @Autowired
+    MemberService memberService;
+
+    @ApiOperation("获取成员")
+    @GetMapping("/member/{id}")
+    public MemberDO member(@PathVariable("id")int id){
+        return memberService.findById(id);
+    }
+
+    @ApiOperation("删除成员")
+    @DeleteMapping("/member/{id}")
+    public int deleteMember(@PathVariable("id") int id){
+        return memberService.deleteById(id);
+    };
+
+    @ApiOperation("添加成员")
+    @PostMapping(value = "/member")
+    public int addMember(@NotNull @RequestBody(required = true) MemberDO memberDO){
+        System.out.println(memberDO.getMemName());
+        int newid =memberService.addOne(memberDO);
+        System.out.println(newid);
+        return newid;
+    }
+
+    @ApiOperation("获取某类成员")
+    @GetMapping("/members/{role}")
+    public List<MemberDO> members(@PathVariable @NotNull String role){
+        return memberService.findAllByRole(role);
+    }
+}
